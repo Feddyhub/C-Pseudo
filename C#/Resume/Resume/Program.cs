0febabf5627639b3640bf4cb7e073334550aeda9
Murat Ferhat Derya <91869273+Feddyhub@users.noreply.github.com>
2024-03-22 21:48:28 +0300

    Add files via upload

---
+++ C#/Resume/Resume/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Runtime.CompilerServices;
+
+public class Resume {
+
+    public  void Main(String[] args)
+    {
+        string temp = Console.ReadLine();
+        yas = int.Parse(temp);
+    }
+     
+    private int yas
+    {
+        get
+        {
+            return yas;
+        }
+        set
+        {
+            if (17 == value)
+            {
+                yas *= 2;
+            }
+        }
+    }
+
+
+}
+
+
---
+++ C#/Vehicle/Vehicle/Program.cs
@@ -0,0 +1,39 @@
+﻿public class Vehicle
+{
+    int HIZ;
+    public Vehicle(int _yil, int _hiz)
+    {
+        hiz = _hiz;
+
+    }
+    public int hiz
+    {
+        get { return HIZ; }
+        set
+        {
+            if (value <=250) 
+            {
+                Console.WriteLine("hizin +10 artti");
+                HIZ = value+10;
+            }
+        }
+
+    }
+    public static void Main(string[] args)
+    {
+        Vehicle Audi = new Vehicle(2012, 320);
+        Console.WriteLine(Audi.hiz);
+        Vehicle honda = new Motorcycle(2008,300);
+    }
+}
+
+public class Motorcycle:Vehicle
+{
+    public Motorcycle(int _yil,int _hiz) : base(_yil,_hiz)
+    {
+
+    }
+
+
+}
+
